5bd26ab1a0bae0424ca86bd3b4e329d2c3d6a080
Amit Kumar <amitcute3@gmail.com>
2023-04-15 13:12:10 +0530

    Refactored index.ts

--- index.ts
+++ index.ts
@@ -1,37 +1,39 @@
 type Json = {
-    [key:string]: any,
-}
+  [key: string]: any;
+};
 
 type SubscriberOption<T> = {
-    func:(val:T) => any,
-    dependencies: string[],
-    isImmediate?:boolean,
-    key?:string,
+  func: (val: T) => any;
+  dependencies: string[];
+  isImmediate?: boolean;
+  key?: string;
 };
 
 type PipedObservables<T> = {
-    subscribe: (options:SubscriberOption<T>) => void,
-    unsubscribe: (key?:string) => void,
-    reset: () => void,
+  subscribe: (options: SubscriberOption<T>) => void;
+  unsubscribe: (key?: string) => void;
+  reset: () => void;
 };
 
 class StateObservables<T> {
-    private initialValue:T;
+    private initialValue: T;
 
-    private value:T;
+    private value: T;
 
-    private isBehaviorObservable:boolean;
+    private isBehaviorObservable: boolean;
 
-    private subscribers:{[key:string]: { func: (val:T) => any, dependencies: Json }} = {};
+    private subscribers: {
+    [key: string]: { func: (val: T) => any; dependencies: Json };
+  } = {};
 
-    constructor(value:Json = {}, isBehaviorObservable = false) {
+    constructor(value: Json = {}, isBehaviorObservable = false) {
         this.initialValue = value as T;
         this.value = value as T;
         this.isBehaviorObservable = isBehaviorObservable;
     }
 
     // eslint-disable-next-line class-methods-use-this
-    private array2Json(arr:string[]) {
+    private array2Json(arr: string[]) {
         const map = {};
 
         arr.forEach((item) => {
@@ -44,7 +46,7 @@ class StateObservables<T> {
     // check if any key of source exists in target
     // eslint-disable-next-line class-methods-use-this
     private compareJsonKeys(source, target) {
-        // if source is empty then return true
+    // if source is empty then return true
         if (Object.keys(source).length < 1) {
             return true;
         }
@@ -63,10 +65,12 @@ class StateObservables<T> {
     private isFunction(x) {
         const type = Object.prototype.toString.call(x);
 
-        return type === '[object Function]'
-            || type === '[object AsyncFunction]'
-            || type === '[object GeneratorFunction]'
-            || type === '[object AsyncGeneratorFunction]';
+        return (
+            type === '[object Function]'
+      || type === '[object AsyncFunction]'
+      || type === '[object GeneratorFunction]'
+      || type === '[object AsyncGeneratorFunction]'
+        );
     }
 
     private validateObject(x) {
@@ -80,11 +84,11 @@ class StateObservables<T> {
     // Broadcast value update to all subscribers
     // empty dependency list means execute on every update.
     // when overwriting all subscribers will be called
-    private broadcast(dependencies:Json, isOverwriting = false) {
+    private broadcast(dependencies: Json, isOverwriting = false) {
         Object.keys(this.subscribers).forEach(async (key) => {
             if (
                 isOverwriting
-                || this.compareJsonKeys(this.subscribers[key].dependencies, dependencies)
+        || this.compareJsonKeys(this.subscribers[key].dependencies, dependencies)
             ) {
                 this.subscribers[key].func(this.value as T);
             }
@@ -95,11 +99,11 @@ class StateObservables<T> {
     // Keys in value determines what dependencies are being updated to broadcast
     // If value is a function, it will be provided with the current state and
     // keys in returned value will be used to trigger dependencies update
-    next(value:T|((x:T) => T), overwrite = false) {
-        let nextVal:T = value as T;
+    next(value: T | ((x: T) => T), overwrite = false) {
+        let nextVal: T = value as T;
 
         if (this.isFunction(value)) {
-            const valFun:any = value;
+            const valFun: any = value;
             nextVal = valFun(this.value);
         }
 
@@ -111,10 +115,14 @@ class StateObservables<T> {
     // register a subscriber which will be executed if value update has dependency changed
     // empty dependency list means execute on every update.
     subscribe({
-        func, isImmediate = this.isBehaviorObservable, key = 'main', dependencies,
-    }:SubscriberOption<T>) {
+        func,
+        isImmediate = this.isBehaviorObservable,
+        key = 'main',
+        dependencies,
+    }: SubscriberOption<T>) {
         this.subscribers[key] = {
-            func, dependencies: this.array2Json(dependencies),
+            func,
+            dependencies: this.array2Json(dependencies),
         };
 
         if (isImmediate) {
@@ -141,12 +149,12 @@ class StateObservables<T> {
 
     // send protected version of observable which only allows limited functionality
     // use this to protect observable value from being updated directly and allow only updates
-    pipe():PipedObservables<T> {
-        return ({
+    pipe(): PipedObservables<T> {
+        return {
             subscribe: this.subscribe.bind(this),
             unsubscribe: this.unsubscribe.bind(this),
             reset: this.reset.bind(this),
-        });
+        };
     }
 }
 
